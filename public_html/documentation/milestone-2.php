bd26e022bb589d10b0354f64ff797ea6285f3177
Thomas Dameron <tdameron1@cnm.edu>
2020-01-28 18:49:07 -0700

    milestone-2 content strategy first draft completed.

---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="eng">
+<head>
+	<meta charset="utf-8">
+	<title>Milestone 2</title>
+
+<body>
+	<h1>Content Strategy</h1>
+
+	My website will consist of one page and will include the following section.
+
+<p><strong>Introduction: </strong> This will include my name and email as well as a quick summary of what my work consists of as well as the kind of work I am looking for.</p>
+
+<p><strong>Portfolio: </strong>This section will contain ~3 subsections that will consist of artwork in three separate categories with each piece labelled with the medium and subject matter. I am planning on having this section consist of three windows where the user can click on an arrow to cycle through pieces of artwork for each given section. For instance, I will have one window that will show paintings and the user can click on the arrows to cycle through different paintings in my portfolio, the same goes for the other sections showcasing drawings or digital artwork etc.</p>
+
+<p><strong>About me: </strong>Here I will list my experiences and education as an artist. This is going to go below the portfolio as personal information is not as important as portfolio pieces. This will be fairly succinct in order to put most of the focus on the portfolio pieces.</p>
+
+<p><strong>Contact: </strong>This will include a contact form as well as my email again.</p>
+
+<p><strong>Social Media: </strong>This will contain links to my social media accounts, such as twitter, and Art Station.</p>
